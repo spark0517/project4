de4633415b6895f00297317d95257a5187e55672
Shawn Park <spark0517@Shawns-MacBook-Pro.local>
2021-11-22 21:23:41 -0700

    signup p1

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,14 +1,113 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ErrorMessage from '../../components/ErrorMessage/ErrorMessage';
-
+import { Button, Form, Grid, Header, Image, Segment } from "semantic-ui-react";
+import userService from '../../utils/userService';
 
 export default function SignUpPage(props){
-    
-    return (
-        <>
-          <h1>Sign up Page!</h1>
 
-          
-        </>
-      );
+  const [error, setError] = useState('')
+  const [state, setState] = useState({
+    username: '',
+    email: '',
+    password: '',
+    passwordConf: '',
+    bio: ''
+  })
+
+  const [selectedFile, setSelectedFile] = useState('')
+
+  function handleChange(e){
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    })
+  }
+  
+  async function handleSubmit(e){
+    e.preventDefault()
+    const formData = new FormData();
+    formData.append('photo', selectedFile)
+
+    for (let key in state){
+      formData.append(key, state[key])
+    }
+
+    try {
+      await userService.signup(formData)
+
+    } catch(err){
+      setError(err.message)
+    }
+
+  }
+
+  function handleFileInput(e){
+    console.log(e.target.files)
+    setSelectedFile(e.target.files[0])
+  }
+
+
+    return (
+      <Grid textAlign="center" style={{ height: "100vh" }} verticalAlign="middle">
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as="h2" color="teal" textAlign="center">
+          <Image src="https://i.imgur.com/s4LrnlU.png" /> Sign Up
+        </Header>
+        <Form autoComplete="off" onSubmit={handleSubmit}>
+          <Segment stacked>
+            <Form.Input
+              name="username"
+              placeholder="username"
+              value={state.username}
+              onChange={handleChange}
+              required
+            />
+            <Form.Input
+              type="email"
+              name="email"
+              placeholder="email"
+              value={state.email}
+              onChange={handleChange}
+              required
+            />
+            <Form.Input
+              name="password"
+              type="password"
+              placeholder="password"
+              value={state.password}
+              onChange={handleChange}
+              required
+            />
+            <Form.Input
+              name="passwordConf"
+              type="password"
+              placeholder="Confirm Password"
+              value={state.passwordConf}
+              onChange={handleChange}
+              required
+            />
+            <Form.TextArea
+              label="bio"
+              name="bio"
+              value={state.bio}
+              placeholder="Tell us more about your dogs..."
+              onChange={handleChange}
+            />
+            <Form.Field>
+              <Form.Input
+                type="file"
+                name="photo"
+                placeholder="upload image"
+                onChange={handleFileInput}
+              />
+            </Form.Field>
+            <Button type="submit" className="btn">
+              Signup
+            </Button>
+          </Segment>
+          {error ? <ErrorMessage error={error} /> : null}
+        </Form>
+      </Grid.Column>
+    </Grid>
+    );
 }
